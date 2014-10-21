619d9e1f8f2b504b457d00970378ea4e69dbd63f
Anderson Nguyen <anguyen135@gmail.com>
2014-10-20 21:39:40 -0700

    Initial Commit

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,8 +1,24 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 NormalParticle[] particleNormal;
 OddballParticle particleOdd;
 JumboParticle particleJumbo;
 
-void setup()
+public void setup()
 {
     size(500, 500);
     particleNormal = new NormalParticle[500];
@@ -11,11 +27,11 @@ void setup()
 
     for (int i = 0; i < particleNormal.length; i++)
     {
-        particleNormal[i] = new NormalParticle(255, 255, 5.0);
+        particleNormal[i] = new NormalParticle(255, 255, 5.0f);
     }
 }
 
-void draw()
+public void draw()
 {   
     background(0);
     for (int i = 0; i < particleNormal.length; i++)
@@ -62,7 +78,7 @@ class NormalParticle implements andersonParticles
         initialize(x, y, particleSize);
     }
 
-    void initialize(double x, double y, float particleSize)
+    public void initialize(double x, double y, float particleSize)
     {
         colorR = ((int)(Math.random()*256)+1);
         colorG = ((int)(Math.random()*256)+1);
@@ -74,19 +90,19 @@ class NormalParticle implements andersonParticles
         radius = sizeParticle/2;        
     }
 
-    void show()
+    public void show()
     {
         fill(colorR, colorG, colorB);
         ellipse((float)xLocation, (float)yLocation, sizeParticle, sizeParticle);
     }
     
-    void move()
+    public void move()
     { 
         yLocation = yLocation + Math.sin(angle);
         xLocation = xLocation + Math.cos(angle);
     }
 
-    void reset()
+    public void reset()
     {
         if (xLocation > 500 + radius || xLocation < 0 - radius|| yLocation > 500 + radius|| yLocation < 0 - radius)
             initialize(originalX, originalY, originalSize);
@@ -100,10 +116,10 @@ class OddballParticle extends NormalParticle
         super(x, y, 30);
     }
     
-    void move()
+    public void move()
     {
-        xLocation = xLocation + ((float)(Math.random()*5) - 2.5);
-        yLocation = yLocation + ((float)(Math.random()*5) - 2.5);
+        xLocation = xLocation + ((float)(Math.random()*5) - 2.5f);
+        yLocation = yLocation + ((float)(Math.random()*5) - 2.5f);
     }
 }
 
@@ -113,4 +129,13 @@ class JumboParticle extends NormalParticle
     {
         super(x, y, 300);
     }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
